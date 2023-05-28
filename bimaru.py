cc40166f11988c593fb6c88ddfcd221284684653
aqfranco <andre.q.franco@gmail.com>
2023-05-28 15:48:14 +0100

    Function result finished and added clear_nearby_positions

--- bimaru.py
+++ bimaru.py
@@ -80,8 +80,8 @@ class Board:
             x = x + 1
         board = Board(board_array, row, column)
         return board
-
-    def adjacent_positions_empty(self, row: int, col: int, type: int) -> int:
+    
+    def adjacent_positions_empty(self, row: int, col: int):
         n = 0
         for i in range(row - 1, row + 2, 1):
             if i < 0:
@@ -92,11 +92,44 @@ class Board:
                 if j < 0 or j > 9 or (i == row and j == col):
                     continue
                 if self.board[i][j] != '' and self.board[i][j] != '.' and self.board[i][j] != 'W':   
-                    if type == 0:
-                        n = n + 1
-                    if type == 1:
-                        self.board[i][j] == '.'
+                    n = n + 1
         return n
+
+    def clear_nearby_positions(self, row: int, col: int, size: int, vertical: bool):
+        if vertical:
+            for i in range(row - 1, row + size + 1, 1):
+                if i < 0:
+                    continue
+                if i > 9:
+                    break
+                for j in range(col - 1, col + 2, 1):
+                    if j < 0 or j > 9 or (i == row and j == col):
+                        continue
+                    if self.board[i][j] == '':
+                        self.board[i][j] = '.'
+        if not vertical:
+            for i in range(row - 1, row + 2, 1):
+                if i < 0:
+                    continue
+                if i > 9:
+                    break
+                for j in range(col - 1, col + size + 1, 1):
+                    if j < 0 or j > 9 or (i == row and j == col):
+                        continue
+                    if self.board[i][j] == '':   
+                        self.board[i][j] = '.'
+    
+    def fill_full_rows(self):
+        for i in range(10):
+            if self.row[i] == 0:
+                for j in range(10):
+                    if self.board[i][j] == '':
+                        self.board[i][j] = '.'
+            if self.column[i] == 0:
+                for j in range(10):
+                    if self.board[j][i] == '':
+                        self.board[j][i] = '.'
+        return 0
 
     def print(self):
         for i in range(10):
@@ -122,30 +155,45 @@ class Bimaru(Problem):
         pass
 
     def result(self, state: BimaruState, action):
-        if action[2] == 1:
-            state.board.board[action[0][0]][action[0][1]] = 'C'
-            state.board.adjacent_positions_empty(action[0][0], action[0][1], 1)
-            state.board.row[action[0][0]] = state.board.row[action[0][0]] - 1
-            state.board.column[action[0][1]] = state.board.column[action[0][1]] - 1
-        """Retorna o estado resultante de executar a 'action' sobre
-        'state' passado como argumento. A ação a executar deve ser uma
-        das presentes na lista obtida pela execução de
-        self.actions(state)."""
-        self.fill_full_rows(state)
-        # TODO
-        pass
-
-    def fill_full_rows(self, state: BimaruState):
-        for i in range(10):
-            if self.board.row[i] == 0:
-                for j in range(10):
-                    if self.board.board[i][j] == '':
-                        self.board.board[i][j] = '.'
-            if self.board.column[i] == 0:
-                for j in range(10):
-                    if self.board.board[j][i] == '':
-                        self.board.board[j][i] = '.'
-        return 0
+        posx_i = action[0][0]
+        posy_i = action[0][1]
+        posx_f = action[1][0]
+        posy_f = action[1][1]
+        size = action[2]
+        if size == 1:
+            state.board.board[posx_i][posy_i] = 'C'
+            state.board.clear_nearby_positions(posx_i, posy_i, size, False)
+            state.board.row[posx_i] = state.board.row[posx_i] - 1
+            state.board.column[posy_i] = state.board.column[posy_i] - 1
+            return 0
+        if posx_i == posx_f:
+            if size >= 3:
+                state.board.board[posx_i][posy_i + 1] = 'M'
+                state.board.column[posy_i + 1] = state.board.column[posy_i + 1] - 1
+                if size == 4:
+                    state.board.board[posx_i][posy_i + 2] = 'M'
+                    state.board.column[posy_i + 2] = state.board.column[posy_i + 2] - 1
+            state.board.board[posx_i][posy_i] = 'L'
+            state.board.board[posx_f][posy_f] = 'R'
+            state.board.row[posx_i] = state.board.row[posx_i] - size
+            state.board.column[posy_i] = state.board.column[posy_i] - 1
+            state.board.column[posy_f] = state.board.column[posy_f] - 1
+            state.board.clear_nearby_positions(posx_i, posy_i, size, False)
+        if posy_i == posy_f:
+            if size >= 3:
+                state.board.board[posx_i + 1][posy_i] = 'M'
+                state.board.row[posx_i + 1] = state.board.row[posx_i + 1] - 1
+                if size == 4:
+                    state.board.board[posx_i + 2][posy_i] = 'M'
+                    state.board.row[posx_i + 2] = state.board.row[posx_i + 2] - 1
+            state.board.board[posx_i][posy_i] = 'T'
+            state.board.board[posx_f][posy_f] = 'B'
+            state.board.column[posy_i] = state.board.column[posy_i] - size
+            state.board.row[posx_i] = state.board.row[posx_i] - 1
+            state.board.row[posx_f] = state.board.row[posx_f] - 1
+            state.board.clear_nearby_positions(posx_i, posy_i, size, True)
+        self.board.fill_full_rows()
+        return state
 
     def goal_test(self, state: BimaruState):
         size1 = 0
@@ -180,7 +228,9 @@ class Bimaru(Problem):
                             return False
                         if size4 < 1:
                             size4 = size4 + 1
-        return True
+        if size1 == 4 and size2 == 3 and size3 == 2 and size4 == 1:
+            return True
+        return False
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
@@ -192,18 +242,18 @@ class Bimaru(Problem):
         j = col
         piece = self.board.board[i][j]
         if piece == 'C':
-            if self.board.adjacent_positions_empty(i, j, 0) != 0:
+            if self.board.adjacent_positions_empty(i, j) != 0:
                 return -1
             return 1
         if piece == 'M':
-            if self.board.adjacent_positions_empty(i, j, 0) != 2:
+            if self.board.adjacent_positions_empty(i, j) != 2:
                 return -1
             HBorders = self.board.adjacent_horizontal_values(i+1, j+1)
             VBorders = self.board.adjacent_vertical_values(i+1, j+1)
             if VBorders != ('T', 'B') and VBorders != ('T', 'M') and VBorders != ('M', 'B') and HBorders != ('L', 'M') and HBorders != ('L', 'R') and HBorders != ('M', 'R'):
                 return -1
         if piece != 'C' and piece != 'M':
-            if self.board.adjacent_positions_empty(i, j, 0) != 1:
+            if self.board.adjacent_positions_empty(i, j) != 1:
                 return -1
             if piece == 'L':
                 RBorder = self.board.board[i][j+1]
@@ -238,15 +288,14 @@ class Bimaru(Problem):
 
 if __name__ == "__main__":
     board = Board.parse_instance()
-    # TODO:
+    #board.fill_full_rows()
     # Ler o ficheiro do standard input,
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
     bimaru = Bimaru(board)
     bimaru_state = BimaruState(board)
-    #bimaru.result(bimaru_state, ((0, 0), (0, 0), 1))
+    #bimaru.result(bimaru_state, ((0, 0), (2, 0), 3))
     bimaru_state.board.print()
     print(bimaru.goal_test(bimaru_state))
-
     pass
